bec6a46e0dee016a4729cd4297fc029381fe009a
Michael Gitonga <gitoshmbae@gmail.com>
2022-08-05 08:54:23 +0300

    changes to urls files, backed to the old format hence the commented out code

--- src/pods_api/apps/connections/urls.py
+++ src/pods_api/apps/connections/urls.py
@@ -1,6 +1,16 @@
+from django.db import router
 from django.urls import path
+from django.conf.urls.static import static
+from django.conf import settings
 from .views import *
+from rest_framework.routers import DefaultRouter
 from rest_framework.urlpatterns import format_suffix_patterns
+
+# router=DefaultRouter()
+# router.register(r'connections',connections_list,basename='connections')
+# router.register(r'connections-detail',connection_detail,basename='connection-detail')
+
+# urlpatterns=router.urls+static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
 
 urlpatterns=[
     path('connections/',connections_list),
--- src/pods_api/apps/podcasts/urls.py
+++ src/pods_api/apps/podcasts/urls.py
@@ -1,6 +1,16 @@
+from django.db import router
 from django.urls import path
+from django.conf.urls.static import static
+from django.conf import settings
 from .views import *
+from rest_framework.routers import DefaultRouter
 from rest_framework.urlpatterns import format_suffix_patterns
+
+# router=DefaultRouter()
+# router.register(r'podcasts',podcasts_list,basename='podcasts')
+# router.register(r'podcasts-detail',podcast_detail,basename='podcast-detail')
+
+# urlpatterns=router.urls+static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
 
 urlpatterns=[
     path('podcasts/',podcasts_list),
